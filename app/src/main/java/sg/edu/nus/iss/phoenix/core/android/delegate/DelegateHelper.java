2fdaa8ef3f16c0e7da6e8cbd39b3ceadc7d6d04c
sugikushal <thamarai131>
2018-09-14 14:19:32 +0800

    Initial commit 1

---
+++ app/src/main/java/sg/edu/nus/iss/phoenix/core/android/delegate/DelegateHelper.java
@@ -0,0 +1,7 @@
+package sg.edu.nus.iss.phoenix.core.android.delegate;
+
+public class DelegateHelper {
+    // RESTful parameters.
+    public final static String PRMS_BASE_URL_AUTHENTICATE = "http://172.17.145.182:37273/phoenix/rest/Login/doLogin?";
+    public final static String PRMS_BASE_URL_RADIO_PROGRAM = "http://172.17.145.182:37273/phoenix/rest/radioprogram";
+}
